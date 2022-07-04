5f3181ee964d0b167b9cfbdde3a61a60a690cae7
Georgi Ovcharov <georgi.ovcharov12@gmail.com>
2022-07-04 14:40:37 +0300

    excluded models from tsconfig

--- dist/models/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BatteryType = void 0;
-var BatteryType;
-(function (BatteryType) {
-    BatteryType["LiIon"] = "Li-Ion";
-    BatteryType["NiMH"] = "NiMH";
-    BatteryType["NiCD"] = "NiCD";
-})(BatteryType = exports.BatteryType || (exports.BatteryType = {}));
